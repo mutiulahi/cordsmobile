2d1abd19a00905e55f34ae69a32a87069f908149
mutiulahi <tesleemolamilekan902@gmail.com>
2023-02-09 17:36:36 +0100

    first time

---
+++ index.php
@@ -0,0 +1,271 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cords Mobile - Power Scheduling</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+    <link rel="shortcut icon" href="assets/icon/favicon.png">
+    <link rel="apple-touch-icon-precomposed" href="assets/icon/icon.png">
+</head>
+
+<body class="counter-scroll home1 home header-fixed">
+    <div class="preload preload-container">
+        <div class="preload-logo"></div>
+    </div>
+    <div id="wrapper">
+        <?php include 'layout/header.php'; ?>
+        <section class="slider">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="swiper-container mainslider">
+                        <div class="swiper-wrapper">
+                            <div class="swiper-slide">
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="content breadcrumbs-wrap">
+                                            <div class="heading title">
+                                                <h1 class="title">Power Status Real-Time Update</h1>
+                                                <p class="font style">Get the current status of the power light update in your environment</p>
+                                            </div>
+
+                                            <div class="button-slider">
+                                                <a class="button-header border-corner" href="servicedetail.html">Join Us</a>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="swiper-slide">
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="content breadcrumbs-wrap">
+                                            <div class="heading title">
+                                                <h1 class="title">Direct Complaint</h1>
+                                                <p class="font style">Make a complaint directly to the power supplier (feeders) </p>
+                                            </div>
+
+                                            <div class="button-slider">
+                                                <a class="button-header border-corner" href="servicedetail.html"> Register</a>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="swiper-slide">
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="content breadcrumbs-wrap">
+                                            <div class="heading title">
+                                                <h1 class="title">Observations</h1>
+                                                <p class="font style">Make a suggestions that will be effected on in short time</p>
+                                            </div>
+
+                                            <div class="button-slider">
+                                                <a class="button-header border-corner" href="servicedetail.html"> Login</a>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="button-custom-slider design-style">
+                        <div class="btn-next-main swiper-button-next button-slide-custom-next">
+                        </div>
+                        <div class="btn-prev-main swiper-button-prev button-slide-custom-prev">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="prochudure">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-12">
+                        <div class="wrap-title-prochudure">
+                            <div class="content">
+                                <div class="inner">
+                                    <p class="desc">What We Do?</p>
+                                    <h3>Our Services</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-4">
+                        <div class="wrap-prochudure">
+                            <div class="content-prochudure ">
+                                <h5><a href="#">Power Update</a></h5>
+                                <p>Get a real time power status of your area.</p>
+                                <hr>
+                                <a href="#" class="btn btn-success">Subscribe</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-xl-4 col-lg-4 col-md-4">
+                        <div class="wrap-prochudure style-prochudure">
+                            <!-- <div class="icon-prochudure style-prochudure"> -->
+                            <!-- <img src="assets/icon/icon-prochudure2.png" alt="image"> -->
+                            <!-- </div> -->
+                            <div class="content-prochudure">
+                                <h5><a href="#">Complaint</a></h5>
+                                <p>Make complaint on the problem on your power status</p>
+                                <hr>
+                                <a href="#" class="btn btn-success">Subscribe</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-xl-4 col-lg-4 col-md-4">
+                        <div class="wrap-prochudure style-prochudure2">
+                            <!-- <div class="icon-prochudure style-prochudure"> -->
+                            <!-- <img src="assets/icon/icon-prochudure3.png" alt="image"> -->
+                            <!-- </div> -->
+                            <div class="content-prochudure">
+                                <h5><a href="#">Observations</a></h5>
+                                <p>Make a suggestion on what you need in your area.</p>
+                                <hr>
+                                <a href="#" class="btn btn-success">Subscribe</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="FAQ">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 col-md-12">
+                        <div class="faq-img">
+                            <img class="image-1" src="assets/images/faq/faq1.jpg" alt="image">
+                            <div class="image-top">
+                                <img class="image-2" src="assets/images/faq/faq2.jpg" alt="image">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-12">
+                        <div class="faq-content">
+                            <div class="wrap-title-FAQ">
+                                <div class="content">
+                                    <div class="inner style">
+                                        <p class="desc faq">FAQ</p>
+                                        <h3>Frequiently Asked
+                                            Question.</h3>
+                                    </div>
+                                    <div class="flat-accordion animated-effect tfanimated">
+                                        <div class="flat-toggle">
+                                            <h6 class="toggle-title"><span>01.</span>How to get your services?</h6>
+                                            <p class="toggle-content">Business analysis and
+                                                process mapping through architecture
+                                                design and data flow modeling to web resource integration with
+                                                various online services business systems.</p>
+                                        </div>
+                                        <div class="flat-toggle">
+                                            <h6 class="toggle-title"><span>02.</span>How to get your services?</h6>
+                                            <p class="toggle-content">Business analysis and
+                                                process mapping through architecture
+                                                design and data flow modeling to web resource integration with
+                                                various online services business systems.</p>
+                                        </div>
+                                        <div class="flat-toggle active">
+                                            <h6 class="toggle-title active"><span>03.</span>How to get your services?</h6>
+                                            <p class="toggle-content">Business analysis and process mapping through
+                                                architecture
+                                                design and data flow modeling to web resource integration with
+                                                various online services business systems.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="a-call">
+            <h1>Call To Action</h1>
+            <div class="overlay"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-12">
+                        <div class="wrap-title-a-call">
+                            <div class="overlay"></div>
+                            <div class="content">
+                                <div class="inner">
+                                    <p class="desc desc-a-call">Request A Call</p>
+                                    <h3>You Can Easily
+                                        Get A Call Back.</h3>
+                                    <form action="#" method="post" id="commentform" class="comment-form" novalidate="novalidate">
+                                        <div class="text-wrap clearfix">
+                                            <fieldset class="name-wrap">
+                                                <input type="text" id="author" class="tb-my-input" name="author" tabindex="1" placeholder="Name:" value="" size="32" aria-required="true">
+                                            </fieldset>
+                                            <fieldset class="email-wrap">
+                                                <input type="email" id="email" class="tb-my-input" name="email" tabindex="2" placeholder="Email:" value="" size="32" aria-required="true">
+                                            </fieldset>
+                                            <fieldset class="phone-wrap">
+                                                <input type="text" id="phone" class="tb-my-input" name="Phone" tabindex="1" placeholder="Phone:" value="" size="32" aria-required="true">
+                                            </fieldset>
+                                            <fieldset class="Address-wrap">
+                                                <input type="text" id="Address" class="tb-my-input" name="Address" tabindex="2" placeholder="Address:" value="" size="32" aria-required="true">
+                                            </fieldset>
+                                        </div>
+                                        <fieldset class="message-wrap">
+                                            <textarea id="comment-message" placeholder="Message..."></textarea>
+                                        </fieldset>
+                                        <p class="form-submit">
+                                            <button class="font-style linear-color border-corner submit"> Submit
+                                                Now</button>
+                                        </p>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <div id="bottom" class="bottom-type1 position-relative">
+            <div class="container">
+                <div class="bottom-wrap text-center">
+                    <div id="copyright">
+                        <a href="#">Copyright © 2020 Themesflat </a><span class="license"> All Rights
+                            Reserved</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <a id="scroll-top"></a>
+
+
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="assets/js/swiper-bundle.min.js"></script>
+    <script src="assets/js/swiper.js"></script>
+    <script src="assets/js/main.js"></script>
+    <script src="assets/js/jquery-validate.js"></script>
+    <script src="assets/js/jquery.easing.js"></script>
+    <script src="assets/js/plugin.js"></script>
+    <script src="assets/js/countto.js"></script>
+    <script src="assets/js/jquery.fancybox.js"></script>
+
+</body>
+
+
+<!-- Mirrored from themesflat.co/html/soffets/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 09 Feb 2023 02:47:21 GMT -->
+
+</html>
